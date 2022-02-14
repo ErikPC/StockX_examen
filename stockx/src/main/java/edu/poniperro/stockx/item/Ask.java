67d5852917df9268bf35cf28d3e66db3f03dac15
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-14 23:10:28 +0100

    feat: crear clase Ask

---
+++ stockx/src/main/java/edu/poniperro/stockx/item/Ask.java
@@ -0,0 +1,32 @@
+package edu.poniperro.stockx.item;
+
+public class Ask implements Offer {
+
+    private String size;
+    private Integer ask;
+
+    public Ask(String size, int ask) {
+        this.size = size;
+        this.ask = ask;
+    }
+
+    @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public int value() {
+        return this.ask;
+    }
+
+    @Override
+    public int compareTo(Offer offer) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "\t\t" + size() + "\t" + value() + "\n";
+    }
+}
