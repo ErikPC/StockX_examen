ee19333deed40fb41d3d8da4faf8f890928174f7
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-14 12:14:27 +0100

    fix: importar lo necesario

--- stockx/src/main/java/edu/poniperro/stockx/criteria/Criteria.java
+++ stockx/src/main/java/edu/poniperro/stockx/criteria/Criteria.java
@@ -2,6 +2,9 @@ package edu.poniperro.stockx.criteria;
 
 import java.util.List;
 
+import edu.poniperro.stockx.item.Item;
+import edu.poniperro.stockx.item.Offer;
+
 public interface Criteria {
     public List<Offer> checkCriteria(Item sneaker);
 }
